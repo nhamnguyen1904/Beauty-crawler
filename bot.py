e2e0bafdb4b64b1510683a4c62a7179191d8d9e1
nhamnguyen1904 <20520664@gm.uit.edu.vn>
2023-05-29 11:25:29 +0700

    fix bug

--- bot.py
+++ bot.py
@@ -9,13 +9,13 @@ import csv
 #Kết nối với trình duyệt
 options = Options()
 options.add_argument('--ignore-certificate-errors')
-browser= webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
 # Create a CSV file to write the product data to
 csv_file = open('data.csv', 'w', newline='', encoding='utf-8')#tạo file product.csv để import sản phẩm
-writer = csv.writer(csv_file,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #viết vô file csv và ngăn cách các trường bởi @
-writer.writerow(["Tiêu đề", "Giá", "Mô tả ngắn", "Mô tả chi tiết"]) #viết vào file csv dòng đầu tiên tiêu đề giá ảnh
+writer = csv.writer(csv_file,delimiter='@', quotechar='"', quoting=csv.QUOTE_MINIMAL) #viết vô file csv và ngăn cách các trường bởi @
+writer.writerow(["Tiêu đề", "Giá", "Mô tả ngắn", "Mô tả chi tiết", "Ảnh"]) #viết vào file csv dòng đầu tiên tiêu đề giá ảnh
 
 
 #Tìm 5 link sản phẩm
@@ -33,7 +33,7 @@ for i in range(1, 10):
 
     products=browser.find_elements(by=By.CLASS_NAME,value='product-item') #lấy ra các sản phẩm
     #Lấy link sản phẩm
-    link_products=[]
+    link_products = []
     for product in products: #lọc qua từng sản phẩm
         link=product.find_element(by=By.CSS_SELECTOR, value='.card-media').get_attribute('href') #lấy link từng sản phẩm
         # nối link
@@ -47,11 +47,10 @@ for i in range(1, 10):
         browser.get(link)
         
         title=browser.find_element(by=By.CSS_SELECTOR, value='.productView-title a').text #Lấy title
-        price=browser.find_element(by=By.CSS_SELECTOR, value='dd.price__last span').text #Lấy giá
+        price = browser.find_element(by=By.CSS_SELECTOR, value='dd.price__last span').text #Lấy giá
         price=price.replace(".","")
         short_description = browser.find_element(by=By.CSS_SELECTOR, value='.productView-desc').text #Lấy short desc
         detail_description = browser.find_element(by=By.CSS_SELECTOR, value='.tab-popup-content').text #Lấy detail
-        print(detail_description)
         #
         # # color_list=browser.find_elements(by=By.CSS_SELECTOR, value='.elc-shade-image-wrapper svg') #Lấy màu
         # # colors = []
@@ -62,16 +61,16 @@ for i in range(1, 10):
         # #         colors.append(name)
         # # colors=','.join(colors)
         #
-        # images=browser.find_elements(by=By.CSS_SELECTOR, value='.productView-thumbnail-link')#Lấy ảnh
-        # image_urls = []
-        # for image in images:
-        #     url = image.get_attribute("src")
-        #
-        #     if url:
-        #         image_urls.append(url)
-        # image_urls=','.join(image_urls)
+        images=browser.find_elements(by=By.CSS_SELECTOR, value='.productView-thumbnail-link img')#Lấy ảnh
+        image_urls = []
+        for image in images:
+            url = image.get_attribute("src")
+
+            if url:
+                image_urls.append(url)
+        image_urls=','.join(image_urls)
         # # Write the product data to the CSV file
-        # writer.writerow([title, price, short_description, detail_description, image_urls])
+        writer.writerow([title, price, short_description, detail_description, image_urls])
 
 
 # sleep(5)
